3a007727b5f45472763f882e6b86f2635eb74fe5
Mesropo <142653068+Mesropo@users.noreply.github.com>
2023-11-13 22:33:32 +0100

    adding Style

---
+++ script.js
@@ -0,0 +1,56 @@
+let questions=[
+  {
+   "question":"Wer ist JavaScript Creator",
+   "answer_1":"Brendan Eich",
+   "answer_2":"Steave Jobs",
+   "answer_3":"Mark Zuckerberg",
+   "answer_4":"Bjarne Stroustrup",
+    "right_answer":1
+  },   
+     
+     {    "question":"Welche ist ein JavaScrit Framework",
+     "answer_1":"Django",
+     "answer_2":"Angular",
+     "answer_3":"FuelPHP",
+     "answer_4":"Laravel",
+      "right_answer":2
+    }, 
+       
+    {    
+        "question":"Wer ist Python  Creator",
+    "answer_1":"Linus Torvalds",
+    "answer_2":"Guido van Rossum",
+    "answer_3":"Bill Gates",
+    "answer_4":"Lary Wall",
+     "right_answer":2
+   }, 
+    
+     {
+   "question":"Wie nennt man null-Variable in JavaScript",
+   "answer_1":"Error",
+   "answer_2":"Garbage",
+   "answer_3":"Not found",
+   "answer_4":"undefined",
+    "right_answer":4
+  },  
+
+  {
+    "question":"Wie nennt man null-Variable in JavaScript",
+    "answer_1":"Error",
+    "answer_2":"Garbage",
+    "answer_3":"Not found",
+    "answer_4":"undefined",
+     "right_answer":4
+   },  
+      
+   {
+   "question":"Wann wurde erste programmieresprache erfunden",
+   "answer_1":"1933",
+   "answer_2":"1985",
+   "answer_3":"1954",
+   "answer_4":"1994",
+    "right_answer":3
+  },  
+     
+    
+]  
